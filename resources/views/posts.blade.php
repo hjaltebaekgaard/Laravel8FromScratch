5c87c4cdcaa737f372ef8b6c246a7cefe30d65b5
Hjalte <hjaltebaekgaard@hotmai.com>
2024-03-18 18:27:43 +0100

    08 - Store Blog Posts as HTML files

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,21 +9,21 @@
 
 <body>
     <article>
-        <h1><a href="/post">First Post</a></h1>
+        <h1><a href="/posts/my-first-post">First Post</a></h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices in iaculis nunc sed augue. Urna molestie at elementum eu facilisis sed odio morbi. Elit ut aliquam purus sit amet luctus venenatis lectus. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate.
         </p>
     </article>
 
     <article>
-        <h1>Second Post</h1>
+        <h1><a href="/posts/my-second-post">Second Post</a></h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices in iaculis nunc sed augue. Urna molestie at elementum eu facilisis sed odio morbi. Elit ut aliquam purus sit amet luctus venenatis lectus. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate.
         </p>
     </article>
 
     <article>
-        <h1>Third Post</h1>
+        <h1><a href="/posts/my-third-post">Third Post</a></h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices in iaculis nunc sed augue. Urna molestie at elementum eu facilisis sed odio morbi. Elit ut aliquam purus sit amet luctus venenatis lectus. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate.
         </p>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
     return view('posts');
 });
 
 Route::get('/post', function () {
-    return view('post');
+    $post = file_get_contents(__DIR__ . '/../resources/posts/my-first-post.html');
+
+    return view('post', [
+        'post' => $post
+    ]);
+});
+
+Route::get('/posts/{post}', function ($slug) {
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    if (!file_exists($path)) {
+        abort(404);
+    }
+    $post = file_get_contents($path);
+
+    return view('post', [
+        'post' => $post
+    ]);
 });
